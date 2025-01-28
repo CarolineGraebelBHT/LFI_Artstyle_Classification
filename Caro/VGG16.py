a6ad1d8b031a116cef6ad4a1d2adf5a3e2c33e36
Caroline Graebel <cagr4985@bht-berlin.de>
2025-01-28 16:55:33 +0100

    Setup VGG-16
    Script for downloading data and first code snippets for running a CNN with VGG-16 architecture

---
+++ Caro/VGG16.py
@@ -0,0 +1,193 @@
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+CATEGORIES = {
+    0: 'Abstract',
+    1: 'Baroque',
+    2: 'Cubism',
+    3: 'Expressionism',
+    4: 'Renaissance',
+    5: 'Impressionism',
+    6: 'Realism'
+}
+
+class MyNeuralNetwork(nn.Module):
+    def __init__(self):
+        super(MyNeuralNetwork, self).__init__()
+        # source: https://medium.com/@mygreatlearning/everything-you-need-to-know-about-vgg16-7315defb5918
+        self.features = nn.Sequential(
+            # first stage
+            nn.Conv2d(1, 64, kernel_size=3, padding= 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding= 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # second stage
+            nn.Conv2d(64, 128, kernel_size=3, padding= 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # third stage
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # fourth stage
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # fifth stage
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(512, 4096),
+            nn.ReLU(True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Linear(4096, 10)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def name(self):
+        return "VGG-16"
+
+def training(model, data_loader, optimizer, criterion, device):
+    model.train()
+
+    running_loss = 0.0
+    running_corrects = 0
+
+    for batch_idx, (inputs, labels) in enumerate(data_loader):
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # forward
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        _, preds = torch.max(outputs, 1)
+
+        # backward
+        loss.backward()
+        optimizer.step()
+
+        # statistics
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+
+        if batch_idx % 10 == 0:
+            print(f'Training Batch: {batch_idx:4} of {len(data_loader)}')
+
+    epoch_loss = running_loss / len(data_loader.dataset)
+    epoch_acc = running_corrects.double() / len(data_loader.dataset)
+
+    print('-' * 10)
+    print(f'Training Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}\n')
+
+    return epoch_loss, epoch_acc
+
+def test(model, data_loader, criterion, device):
+    model.eval()
+
+    running_loss = 0.0
+    running_corrects = 0
+
+    # do not compute gradients
+    with torch.no_grad():
+
+        for batch_idx, (inputs, labels) in enumerate(data_loader):
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # forward
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            _, preds = torch.max(outputs, 1)
+
+            # statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+
+            if batch_idx % 10 == 0:
+                print(f'Test Batch: {batch_idx:4} of {len(data_loader)}')
+
+        epoch_loss = running_loss / len(data_loader.dataset)
+        epoch_acc = running_corrects.double() / len(data_loader.dataset)
+
+    print('-' * 10)
+    print(f'Test Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}\n')
+
+    return epoch_loss, epoch_acc
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# set seed for reproducability
+torch.manual_seed(0)
+
+# hyperparameter
+# TODO: find good hyperparameters
+batch_size = 32
+num_epochs = 30
+learning_rate =  0.01
+momentum = 0.9
+
+transform = transforms.Compose([
+    # you can add other transformations in this list
+    transforms.ToTensor()
+])
+
+# load train and test data
+root = './Data'
---
+++ Caro/download_data.py
@@ -0,0 +1,5 @@
+import kagglehub
+
+path = kagglehub.dataset_download("steubk/wikiart")
+
+print("Path to dataset files:", path)
